17883bef94f6bbf09e9b7bd879698568d74b65e0
Vishal Bhat Soori <vishalbhatsoori@gmail.com>
2025-08-01 13:13:45 +0000

    Final commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,10 @@
+import cors from 'cors';
 import express from 'express';
 import aiRoute from './routes/ai.routes.js';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.get('/',(req,res)=>{
---
+++ frontend/src/App.tsx
@@ -0,0 +1,66 @@
+import axios from "axios";
+//import "highlight.js/styles/github-dark.css";
+import prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
+import { useEffect, useState } from "react";
+import Markdown from "react-markdown";
+import Editor from "react-simple-code-editor";
+//import reHypeHighlight from "rehype-highlight";
+import "./App.css";
+
+function App() {
+  const [code, setCode] = useState(`function sum(){
+     return 1 + 1;
+    }`);
+
+  const [review, setreview] = useState(
+    `Waiting for user to Enter or Paste the Code and Press Review .....`
+  );
+
+  useEffect(() => {
+    prism.highlightAll();
+  });
+
+  async function reviewCode() {
+    setreview("Waiting for Your Code Buddy to review the code .....");
+    const response = await axios.post("http://localhost:3000/ai/post-code", {
+      code,
+    });
+    setreview(response.data);
+  }
+  return (
+    <>
+      <main>
+        <div className="left">
+          <div className="code">
+            <Editor
+              value={code}
+              onValueChange={(code) => setCode(code)}
+              highlight={(code) =>
+                prism.highlight(code, prism.languages.javascript, "javascript")
+              }
+              padding={10}
+              style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 16,
+                border: "1px solid #ddd",
+                borderRadius: "5px",
+                height: "100%",
+                width: "100%",
+                caretColor: "black",
+              }}
+            />
+          </div>
+          <div onClick={reviewCode} className="review">
+            Review
+          </div>
+        </div>
+        <div className="right">
+          <Markdown>{review}</Markdown>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
